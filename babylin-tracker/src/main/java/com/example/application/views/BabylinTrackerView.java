59cf1106fff707189e7ce7674066323c29c8da6c
Ruth Blankfors <ruth.aleks@gmail.com>
2023-09-25 09:49:04 +0200

    Added posibility to edit the time stamp of the event

--- babylin-tracker/src/main/java/com/example/application/views/BabylinTrackerView.java
+++ babylin-tracker/src/main/java/com/example/application/views/BabylinTrackerView.java
@@ -4,11 +4,16 @@ import com.example.application.BabylinTracker;
 import com.example.application.BabylinTrackerRepo;
 import com.example.application.EventType;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datetimepicker.DateTimePicker;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 
 @Route(value = "/log", layout = MainLayout.class)
@@ -35,9 +40,34 @@ public class BabylinTrackerView extends VerticalLayout {
     }
 
     private void addButtonClickListener(Button button, EventType eventType){
+        var dateTimePickerPopup = dateTimePickerPopup(eventType);
         button.addClickListener(click -> {
-            repo.save(new BabylinTracker(eventType, LocalDateTime.now()));
+            dateTimePickerPopup.open();
         });
+        dateTimePickerPopup.addDialogCloseActionListener(event -> dateTimePickerPopup.close());
+    }
+
+    private Dialog dateTimePickerPopup(EventType eventType){
+        Dialog dateTimePickerPopup = new Dialog();
+        dateTimePickerPopup.setModal(true);
+
+        DateTimePicker dateTimePicker = new DateTimePicker("Välj dag och tid");
+        dateTimePicker.setValue(LocalDateTime.now());
+        dateTimePicker.setStep(Duration.ofMinutes(30));
+        dateTimePicker.setLocale(Locale.UK); // to ensure 24h format
+
+
+        Button saveButton = new Button("Spara");
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveButton.addClassName("custom-margin-button");
+
+        saveButton.addClickListener(click -> {
+            repo.save(new BabylinTracker(eventType, dateTimePicker.getValue()));
+            dateTimePickerPopup.close();
+        });
+
+        dateTimePickerPopup.add(dateTimePicker, saveButton);
+        return dateTimePickerPopup;
     }
 
 }
